d1bfdc0e46069f01e86ecec1cacf01a76a585703
KaiOelfke <k.oelfke@me.com>
2012-11-04 00:04:32 +0100

    new read, write, and main

--- U2/aufgabe4.c
+++ U2/aufgabe4.c
@@ -3,25 +3,62 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <errno.h>
+#define BUFFER_SIZE (64 * 1024)
 int copy(char *source, char *target)
 {
-	int fd = open(source, O_RDWR);
-	int fd2 = open(target, O_CREAT,0777);
-  	int clos1 = close( fd2);
-	int fd3 = open(target, O_RDWR);
-  	int seek = lseek( fd, 0L,SEEK_END);
-  	int seek2 = lseek( fd, 0,SEEK_SET);
-	char * buffer = (char*) malloc(seek*sizeof(char));
-	int h = read(fd, buffer, seek);
-	int wr = write(fd3, buffer, seek);
-	free (buffer);
- 	int clos = close(fd);
- 	int clos2 = close(fd3);
+	int fdOriginal = open(source, O_RDONLY);
+	if (fdOriginal < 0)
+	{
+		perror("Konnte Quelle nicht oeffnen:");
+		return 1;
+	}
+	int fdCopy = open(target, O_WRONLY | O_CREAT | O_EXCL | O_EXLOCK, S_IRWXU | S_IRGRP | S_IWGRP | S_IROTH);
+	if (fdCopy < 0)
+	{
+        perror( "Konnte Zieldatei nicht erstellen:");
+		return 1;
+	}
+	char buffer[BUFFER_SIZE];
+	for (;;) 
+	{
+		int bytesRead = read(fdOriginal, buffer, BUFFER_SIZE);
+		if (bytesRead == 0) break;
+		if (bytesRead == -1)
+		{
+			perror("Fehler beim Lesen:");
+			return 1;
+		}
+		char *writePostion = buffer;
+		while (bytesRead > 0)
+		{
+			int bytesWritten = write(fdCopy, buffer, bytesRead);
+			if (bytesWritten == -1)
+			{
+				perror("Fehler beim Schreiben");
+				return 1;
+			}
+			bytesRead -= bytesWritten;
+			writePostion += bytesWritten;
+		}
+	}
+ 	int closeOriginal = close(fdOriginal);
+ 	int closeCopy = close(fdCopy);
+ 	if (closeOriginal == -1 || closeCopy == -1)
+ 	{
+ 		perror("Fehler beim Schließen");
+ 		return 1;
+ 	}
 	return 0;
 }
 
-int main(){
-	char name[] ="ahmed.txt";
-	copy("text.txt",name);
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		perror("Bitte Programm mit: Quelldatei Zieldatei aufrufen\n");
+		return 1;
+	}
+	copy(argv[1],argv[2]);
 	return 0;
 }
