5c869d80afc0829af854d2a86cec0212921d416b
KaiOelfke <k.oelfke@me.com>
2012-12-17 17:22:04 +0100

    it's running

--- U7/aufgabe2.c
+++ U7/aufgabe2.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct directoryEntry
+
+struct __attribute__ ((__packed__)) bootSector
+{
+	char code[3];
+	char nameOfOS[8];
+	short int bytesPerSector; //Usally 512 bytes
+	char sectorsPerCluster;
+	short int reservedSectors;
+	char countFATCopies; //Usally 2
+	short int maxRootEntries; //512 recommended
+	short int smallNumberOfSectors;
+	char mediaDescriptor;
+	short int sectorsPerFAT;
+	short int sectorsPerTrack;
+	short int numberOfHeads;
+	int hiddenSectors;
+	int largeNumberOfSectors;
+	char driveNumber;
+	char reservedForWindowsNT;
+	char extendedBootSignature;
+	int volumeSerialNumber;
+	char volumeLabel[11];
+	char fileSystemType[8];
+	char bootstrapCode[448];
+	short int bootSectorSignature;
+};
+
+typedef struct bootSector BOOTSECTOR;
+
+struct __attribute__ ((__packed__)) directoryEntry
 {
 	char filename[8];
 	char extension[3];
@@ -33,7 +62,7 @@ int readAttributeByte(char byte)
 }
 
 int main(int argc, char *argv[]) {
-	
+	/*
 	short int howmany;
 	int rootDirectoryRegionStart;
 	int dataRegionStart;
@@ -53,77 +82,84 @@ int main(int argc, char *argv[]) {
 	short int bootEntiesCount;
 	int totalNumberOfSectors;
 	int displayme;
-	char string[11] ;
-	FILE* imagefile = fopen(argv[1], "rb");	if (imagefile != NULL)
-		{
-			// Read BytesPerSector
-			fseek(imagefile,0x0B, SEEK_CUR);
-			fread(&bytesPerSector, 2, 1, imagefile);
-			// Read ReservedSectors
-			rewind(imagefile);
-			fseek(imagefile,0x0E, SEEK_CUR);
-			fread(&reservedSectors, 2, 1, imagefile);
-			// Read Number of FAT copies
-			rewind(imagefile);
-			fseek(imagefile,0x10, SEEK_CUR);
-			fread(&numberOfFATs, 1, 1, imagefile);
-			// Read Sectors Per FAT
-			rewind(imagefile);
-			fseek(imagefile,0x016, SEEK_CUR);
-			fread(&sectorsPerFAT, 1, 2, imagefile);
-			// Read Root Enties Count
-			rewind(imagefile);
-			fseek(imagefile,0x011, SEEK_CUR);
-			fread(&rootEntiesCount, 1, 2, imagefile);
-			// Read Reserved Sectors
-			rewind(imagefile);
-			fseek(imagefile,0x0D, SEEK_CUR);
-			fread(&clusterInSector, 1, 1, imagefile);
-			// Read Large number of sectors
-			rewind(imagefile);
-			fseek(imagefile,0x020, SEEK_CUR);
-			fread(&totalNumberOfSectors, 1, 4, imagefile);
-			// file size
-			rewind(imagefile);
-			fseek(imagefile,0x1051B, SEEK_CUR);
-			fread(&displayme, 1, 4, imagefile);
-			//
-			 reservedRegionStart = 0;
-			 fATRegionStart = reservedRegionStart + reservedSectors;
-			 rootDirectoryRegionStart = fATRegionStart +(numberOfFATs*sectorsPerFAT);
-			
-			 dataRegionStart = rootDirectoryRegionStart + ((rootEntiesCount*32)/bytesPerSector);
-			 
-			 reservedRegionSize = reservedSectors;
-			 fatRegionSize = numberOfFATs * sectorsPerFAT;
-			 rootDirectoryRegionSize = (rootEntiesCount*32)/bytesPerSector;
-			 dataRegionSize = totalNumberOfSectors - (reservedRegionStart+fatRegionSize+rootDirectoryRegionSize);
-			for (int i = 0; i < 27; i++)
-			{
-				fseek(imagefile, rootDirectoryRegionStart * bytesPerSector + 32 * i, SEEK_SET);
-							DIRENTRY* rootDir = (DIRENTRY *) malloc(sizeof(DIRENTRY));
-							fread(rootDir, sizeof(DIRENTRY), 1, imagefile);
-							if (readAttributeByte(rootDir->attributeByte))
-							{
-								printf("%s\n",rootDir->filename);
-								printf("%s\n",rootDir->extension);
-							}
-			}
-			
+	char string[11] ;*/
+	FILE* imagefile = fopen(argv[1], "rb"); if (imagefile != NULL)
+	if (imagefile != NULL)
+	{        
+		rewind(imagefile);
+		BOOTSECTOR bootSector;
+		printf("%lu\n",sizeof(BOOTSECTOR));
+		fread(&bootSector, sizeof(BOOTSECTOR), 1, imagefile);
+		printf("Name of OS:%s\nMaxRootEntries:%hd\n",bootSector.nameOfOS, bootSector.maxRootEntries);
 
-			
-			printf("ReservedSectors: %hd \n", reservedSectors);
-			printf("ReservedRegion start :%d  %d\n",reservedRegionStart,displayme );
-			printf("sectorsPerFAT : %hd\n", sectorsPerFAT);
-			printf("bytesPerSector : %hd\n", bytesPerSector);
-			printf("clusterInSector : %d\n", (int)clusterInSector);
-			printf("fATRegionStart  :%d\n",fATRegionStart);
-			printf("rootDirectoryRegionStart  :%d \n",rootDirectoryRegionStart);
-			printf("dataRegionStart  :%d \n",dataRegionStart);
-			printf("reservedRegionSize  :%d \n",reservedRegionSize);
-			printf("fatRegionSize  :%d \n",fatRegionSize);
-			printf("rootDirectoryRegionSize  :%d \n",rootDirectoryRegionSize);
-			printf("dataRegionSize  :%d \n",dataRegionSize);
+
+		/*
+		// Read BytesPerSector
+		fseek(imagefile,0x0B, SEEK_CUR);
+		fread(&bytesPerSector, 2, 1, imagefile);
+		// Read ReservedSectors
+		rewind(imagefile);
+		fseek(imagefile,0x0E, SEEK_CUR);
+		fread(&reservedSectors, 2, 1, imagefile);
+		// Read Number of FAT copies
+		rewind(imagefile);
+		fseek(imagefile,0x10, SEEK_CUR);
+		fread(&numberOfFATs, 1, 1, imagefile);
+		// Read Sectors Per FAT
+		rewind(imagefile);
+		fseek(imagefile,0x016, SEEK_CUR);
+		fread(&sectorsPerFAT, 1, 2, imagefile);
+		// Read Root Enties Count
+		rewind(imagefile);
+		fseek(imagefile,0x011, SEEK_CUR);
+		fread(&rootEntiesCount, 1, 2, imagefile);
+		// Read Reserved Sectors
+		rewind(imagefile);
+		fseek(imagefile,0x0D, SEEK_CUR);
+		fread(&clusterInSector, 1, 1, imagefile);
+		// Read Large number of sectors
+		rewind(imagefile);
+		fseek(imagefile,0x020, SEEK_CUR);
+		fread(&totalNumberOfSectors, 1, 4, imagefile);
+		// file size
+		rewind(imagefile);
+		fseek(imagefile,0x1051B, SEEK_CUR);
+		fread(&displayme, 1, 4, imagefile);
+		//
+		reservedRegionStart = 0;
+		fATRegionStart = reservedRegionStart + reservedSectors;
+		rootDirectoryRegionStart = fATRegionStart +(numberOfFATs*sectorsPerFAT);
+		
+		dataRegionStart = rootDirectoryRegionStart + ((rootEntiesCount*32)/bytesPerSector);
+		 
+		reservedRegionSize = reservedSectors;
+		fatRegionSize = numberOfFATs * sectorsPerFAT;
+		rootDirectoryRegionSize = (rootEntiesCount*32)/bytesPerSector;
+		dataRegionSize = totalNumberOfSectors - (reservedRegionStart+fatRegionSize+rootDirectoryRegionSize);
+		for (int i = 0; i < 27; i++)
+		{
+			fseek(imagefile, rootDirectoryRegionStart * bytesPerSector + 32 * i, SEEK_SET);
+			DIRENTRY* rootDir = (DIRENTRY *) malloc(sizeof(DIRENTRY));
+			fread(rootDir, sizeof(DIRENTRY), 1, imagefile);
+			if (readAttributeByte(rootDir->attributeByte))
+			{
+				printf("%s\n",rootDir->filename);
+				printf("%s\n",rootDir->extension);
+			}
+		}
+		
+		printf("ReservedSectors: %hd \n", reservedSectors);
+		printf("ReservedRegion start :%d  %d\n",reservedRegionStart,displayme );
+		printf("sectorsPerFAT : %hd\n", sectorsPerFAT);
+		printf("bytesPerSector : %hd\n", bytesPerSector);
+		printf("clusterInSector : %d\n", (int)clusterInSector);
+		printf("fATRegionStart  :%d\n",fATRegionStart);
+		printf("rootDirectoryRegionStart  :%d \n",rootDirectoryRegionStart);
+		printf("dataRegionStart  :%d \n",dataRegionStart);
+		printf("reservedRegionSize  :%d \n",reservedRegionSize);
+		printf("fatRegionSize  :%d \n",fatRegionSize);
+		printf("rootDirectoryRegionSize  :%d \n",rootDirectoryRegionSize);
+		printf("dataRegionSize  :%d \n",dataRegionSize);*/
 		}
 
 	fclose(imagefile);
---
+++ U7/myls.c
@@ -0,0 +1,179 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+struct __attribute__ ((__packed__)) bootSector
+{
+	char code[3];
+	char nameOfOS[8];
+	short int bytesPerSector; //Usally 512 bytes
+	char sectorsPerCluster;
+	short int reservedSectors;
+	char numberOfFATs; //Usally 2
+	short int maxRootEntries; //512 recommended
+	short int smallNumberOfSectors;
+	char mediaDescriptor;
+	short int sectorsPerFAT;
+	short int sectorsPerTrack;
+	short int numberOfHeads;
+	int hiddenSectors;
+	int largeNumberOfSectors;
+	char driveNumber;
+	char reservedForWindowsNT;
+	char extendedBootSignature;
+	int volumeSerialNumber;
+	char volumeLabel[11];
+	char fileSystemType[8];
+	char bootstrapCode[448];
+	short int bootSectorSignature;
+};
+
+typedef struct bootSector BOOTSECTOR;
+
+struct __attribute__ ((__packed__)) directoryEntry
+{
+	char filename[8];
+	char extension[3];
+	char attributeByte;
+	char reservedWindowsNT;
+	char creationMSStamp;
+	short int creationTime;
+	short int creationDate;
+	short int lastAccessDate;
+	short int reservedFAT32;
+	short int lastWriteTime;
+	short int lastWriteDate;
+	short int startingCluster;
+	int fileSizeInBytes;
+};
+
+typedef struct directoryEntry DIRENTRY;
+
+void longToShortFileName(char * filename){
+	
+	char* position = NULL;
+	int charCount = 0;
+	int i = 0;
+	int len = strlen(filename);
+	while (charCount < 9 || i < len) {
+			
+		if (filename[i] != ' ')
+			charCount++;
+		i++;
+	}
+	filename[i] = '\0';
+	
+	position = strchr(filename, ' ');
+	
+	while (position != NULL)
+	{
+		strcpy(position, position + 1);
+		position = strchr(filename, ' ');
+	}
+	
+	int fileNameLength = strlen(filename);
+	if (fileNameLength >8){
+		filename[6] = '~';
+		filename[7] = '1';
+		filename[8] = '\0';
+		
+	}	
+}
+
+void splitPath(char* folder, char* path)
+{
+	char* firstSeperator = strchr(path, '/');
+	if (firstSeperator == NULL)
+	{
+		strcpy(folder, path);
+		path[0] = '\0';
+		return;
+	}
+	char* newPath = firstSeperator + 1;
+	int folderLength = strlen(path) - strlen(newPath) - 1;
+	strncpy(folder, path, folderLength);
+	folder[folderLength] = '\0';
+	longToShortFileName(folder);
+	strcpy(path,newPath);
+}
+
+char isFolder(char byte)
+{
+	return byte & 16;
+}
+char readAttributeByte(char byte)
+{
+	byte = byte >> 1;
+	for (int i = 0; i < 3; i++)
+	{
+		if ((byte & 1) == 1)
+			return 0;
+		else
+			byte = byte >> 1;
+	}
+	return 1;
+}
+int main(int argc, char* argv[])
+{
+	if (argc != 3)
+	{
+		perror("Programm muss mit <./myls image directory> aufgerufen werden.");
+		return EXIT_FAILURE;
+	}
+	FILE* imageFile = fopen(argv[1], "r");
+	if (imageFile == NULL)
+	{
+		perror("Konnte image nicht oeffnen.");
+		return EXIT_FAILURE;
+	}
+	BOOTSECTOR bootSector;
+	fread(&bootSector, sizeof(BOOTSECTOR), 1, imageFile);
+	
+	int startReservedRegion = 0;
+	int startFATRegion = startReservedRegion + bootSector.reservedSectors;
+	int startRootDirectoryRegion = startFATRegion + bootSector.numberOfFATs * bootSector.sectorsPerFAT;
+	
+	int startDataRegion = startRootDirectoryRegion + (bootSector.maxRootEntries * 32) / bootSector.bytesPerSector;
+	 
+	int sizeReservedRegion = bootSector.reservedSectors;
+	int sizeFATRegion = bootSector.numberOfFATs * bootSector.sectorsPerFAT;
+	int sizeRootDirectoryRegion = (int) ceil((bootSector.maxRootEntries * 32) / bootSector.bytesPerSector);
+	int totalNumberOfSectors = bootSector.smallNumberOfSectors == 0 ? bootSector.largeNumberOfSectors : bootSector.smallNumberOfSectors;
+	int sizeDataRegion = totalNumberOfSectors - (startReservedRegion + sizeFATRegion + sizeRootDirectoryRegion);
+	
+
+	DIRENTRY entry;
+	char* path = argv[2];
+	char folder[strlen(path)];
+	splitPath(folder,path);
+	char notFinished = 1;
+	int offset = startRootDirectoryRegion * bootSector.bytesPerSector;
+	fseek(imageFile, offset, SEEK_SET);
+	int i = 0;
+	while (fread(&entry, sizeof(DIRENTRY), 1, imageFile) == 1 && notFinished)
+	{
+		switch ((unsigned char) entry.filename[0]) //unsigned char cast, weil sonst 229 zu gross ist. gcc warnung
+		{
+			case '\0':
+				notFinished = 0;
+				break;
+			case 229:
+				break;
+			default:
+				if (folder[0] == '\0' && readAttributeByte(entry.attributeByte))
+					printf("%s\n",entry.filename);
+				else if (readAttributeByte(entry.attributeByte) && isFolder(entry.attributeByte) && strncmp(folder,entry.filename,strlen(folder)) == 0) 
+				{
+					offset = (startDataRegion + (entry.startingCluster - 2) * bootSector.sectorsPerCluster) * bootSector.bytesPerSector;
+					splitPath(folder,path);
+					i = -1;
+					break;
+				}
+				break;
+		} 
+		i++;
+		fseek(imageFile, offset + 32 * i, SEEK_SET);
+	}
+	fclose(imageFile);
+	return EXIT_SUCCESS;
+}
