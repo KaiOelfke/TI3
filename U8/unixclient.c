66657f463ae39c4a74cb35d9a62df6b4972bd963
KaiOelfke <k.oelfke@me.com>
2012-12-18 18:20:10 +0100

    added u8

---
+++ U8/unixclient.c
@@ -0,0 +1,49 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <stdio.h>
+#include <strings.h>
+int main(int argc, char *argv[])
+{
+	int socketFileDescriptor;
+	int serverAddressLength;
+	int n;
+	struct sockaddr_un serverAddress;
+	char buffer[82];
+
+	//bzero ueberschreibt struct mit Nullen
+	bzero((char *)&serverAddress,sizeof(serverAddress));
+	//Wir wollen Unix Domain Sockets
+	serverAddress.sun_family = AF_UNIX;
+	//Pfad fuer den Socket aus Aufruf sun = socket unix != Sonne
+	strcpy(serverAddress.sun_path, argv[1]);
+	//Ermittle Laenge der Serveradresse fuer connect()
+	serverAddressLength = strlen(serverAddress.sun_path) + sizeof(serverAddress.sun_family);
+	/*
+	SOCK_STREAM
+	stellt einen sequenziellen, verlässlichen, zwei-wege, verbindungsbasierten
+	Byte-Stream zur Verfügung. 
+	Ein lqout-of-bandrq Übertragungsmechanismus kann unterstützt werden.
+	http://www.linuxhowtos.org/data/6/U_client.c
+	*/
+	if ((socketFileDescriptor = socket(AF_UNIX, SOCK_STREAM,0)) < 0)
+	{
+		perror("Socket konnte nicht erstellt werden.");
+		exit(0);
+	}
+	if (connect(socketFileDescriptor, (struct sockaddr *) &serverAddress, serverAddressLength) < 0)
+	{
+		perror("Verbindung konnte nicht hergestellt werden.");
+		exit(0);
+	}
+	printf("Eingabe max. 70 Zeichen:\n");
+	bzero(buffer,82);
+	fgets(buffer,80,stdin);
+	write(socketFileDescriptor,buffer,strlen(buffer));
+	n = read(socketFileDescriptor,buffer,80);
+	printf("Antwort:\n%s",buffer);
+	close(socketFileDescriptor);
+	return 0;
+}
---
+++ U8/unixserver.c
@@ -0,0 +1,49 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/un.h>
+#include <stdio.h>
+#include <strings.h>
+
+int main(int argc, char *argv[])
+{
+	int socketFileDescriptor;
+	int newSocketFileDescriptor;
+	int serverAddressLength;
+	int n;
+	struct sockaddr_un serverAddress;
+	struct sockaddr_un clientAddress;
+	socklen_t clientLength;
+	char buffer[80];
+
+	if ((socketFileDescriptor = socket(AF_UNIX,SOCK_STREAM,0)) < 0)
+	{
+		perror("Socket konnte nicht erstellt werden.");
+		exit(0);
+	} 
+	bzero((char *) &serverAddress, sizeof(serverAddress));
+	serverAddress.sun_family = AF_UNIX;
+	strcpy(serverAddress.sun_path, argv[1]);
+	serverAddressLength = strlen(serverAddress.sun_path) + sizeof(serverAddress.sun_family);
+	if(bind(socketFileDescriptor,(struct sockaddr *)&serverAddress,serverAddressLength ) <0)
+	{
+		perror("Konnte Socket nicht binden");
+		exit(0);
+	} 
+
+	listen(socketFileDescriptor,5);
+	clientLength = sizeof(clientAddress);
+	newSocketFileDescriptor = accept(socketFileDescriptor,(struct sockaddr *)&clientAddress,&clientLength);
+	if (newSocketFileDescriptor < 0) 
+	{
+		perror("Konnte Verbindung nicht akzeptieren");
+		exit(0);
+	}
+	n = read(newSocketFileDescriptor,buffer,80);
+	printf("Eine Verbindung wurde hergestellt.\n%s",buffer);
+	write(newSocketFileDescriptor,"Hallo Client\n",13);
+	close(newSocketFileDescriptor);
+	close(socketFileDescriptor);
+	return 0;
+}
